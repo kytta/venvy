e5deea0d96ffd21e7b92b16c5ccdfc888db517be
Nikita Karamov <me@kytta.dev>
2022-12-31 00:55:53 +0100

    Add argument parser

--- venvy/main.py
+++ venvy/main.py
@@ -6,13 +6,31 @@ from typing import Sequence
 
 
 def _get_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="A very opinionated virtualenv wrapper for an even simpler"
+                    "venv management.",
+    )
+
+    parser.add_argument(
+        "query",
+        type=str,
+        nargs="*",
+        default=".venv",
+        metavar="QUERY",
+        help="Magic query argument. Allows you to specify the name, the "
+             "Python distribution or version, or everything at once.",
+    )
+
     return parser
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     argv = argv if argv is not None else sys.argv[1:]
-    return 0
+
+    args = _get_parser().parse_args(argv)
+    print(args)
+
+    return 1
 
 
 if __name__ == "__main_":
