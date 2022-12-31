8859b8b5daa6837b6ec440b1143b0e95d2478b80
Nikita Karamov <me@kytta.dev>
2022-12-31 01:12:43 +0100

    Add --version argument

--- venvy/main.py
+++ venvy/main.py
@@ -4,11 +4,21 @@ import argparse
 import sys
 from typing import Sequence
 
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+
 
 def _get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="A very opinionated virtualenv wrapper for an even "
                     "simpler venv management.",
+    )
+    parser.add_argument(
+        "-V", "--version",
+        action="version",
+        version=version("venvy"),
     )
 
     parser.add_argument(
