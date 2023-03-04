4ad037f34ae103f397eaf1348d74d58203c17878
Nikita Karamov <me@kytta.dev>
2023-03-04 22:20:22 +0100

    Add tests

--- tests/parse_query_test.py
+++ tests/parse_query_test.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from typing import Sequence
 
 import pytest
@@ -7,15 +8,21 @@ import pytest
 from venvy import parse_query
 from venvy import VirtualEnvParams
 
+TESTS_DIR = os.path.dirname(__file__)
 
 # TODO: update derived activators when we detect activators
+
+
 @pytest.mark.parametrize(
     ("query", "expected"), [
         ([], (None, ".venv", [])),
         (["37"], ("37", ".venv", [])),
         (["bash", "3.7"], ("3.7", ".venv", ["bash"])),
         (["pypy", "myvenv"], ("pypy", "myvenv", [])),
-        ([__file__], (__file__, ".venv", [])),
+        (
+            [f"{TESTS_DIR}/resources/executable"],
+            (f"{TESTS_DIR}/resources/executable", ".venv", []),
+        ),
     ],
 )
 def test_parse_query(
