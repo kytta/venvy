8a3d820ddd9e60313670f23929c49c33b385ebe3
Nikita Karamov <me@kytta.dev>
2023-03-05 01:43:56 +0100

    Fix tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -11,7 +11,24 @@ from venvy import main
         (["py37"], 0),
     ],
 )
-def test_main_return_value(argv: list[str], exit_code: int) -> None:
+@pytest.mark.parametrize(
+    "shell", [
+        "bash",
+        "xonsh",
+    ],
+)
+def test_main_return_value(
+    argv: list[str],
+    exit_code: int,
+    shell: str,
+    monkeypatch,
+) -> None:
+    import shellingham
+
+    def return_shell():
+        return shell, f"/usr/local/bin/{shell}"
+
+    monkeypatch.setattr(shellingham, "detect_shell", return_shell)
     assert main(argv) == exit_code
 
 
